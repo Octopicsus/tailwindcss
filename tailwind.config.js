0e043dd2c35f2ab169af7570b5c6b8d4ddd6c96b
OCTO <chobotnicecz@gmail.com>
2024-11-18 15:14:31 +0100

    tailwindcss - start

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+    "./**/*.{html,js}",
+
+    "./**/*.{html,js}",
+    "!./node_modules/**/*.*"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
