41b5cbf583d798c982dcb8f0dd261ca5febe3904
OCTO <chobotnicecz@gmail.com>
2024-11-19 20:40:32 +0100

    tailwindcss - links - 03

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'main-banner-img':"url('/tailwindcss/img/brand-img.png')",
-        'card-01-img':"url('/tailwindcss/tailwindcss/img/img-01.png')",
+        'card-01-img':"url('/tailwindcss/img/img-01.png')",
         'card-02-img':"url('/tailwindcss/img/img-02.png')",
         'card-03-img':"url('/tailwindcss/img/img-03.png')",
         'card-04-img':"url('/tailwindcss/img/img-04.png')",
