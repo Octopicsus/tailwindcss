84305320f5fa97da4908ee354909a034598a66e7
OCTO <chobotnicecz@gmail.com>
2024-11-19 20:15:42 +0100

    tailwindcss - links - 02

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,14 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'main-banner-img':"url('/img/brand-img.png')",
-        'card-01-img':"url('/img/img-01.png')",
-        'card-02-img':"url('/img/img-02.png')",
-        'card-03-img':"url('/img/img-03.png')",
-        'card-04-img':"url('/img/img-04.png')",
-        'card-05-img':"url('/img/img-05.png')",
-        'sale-banner-img':"url('/img/img-banner.png')",
-        'app-banner-img':"url('/img/mobile-app.png')",
+        'main-banner-img':"url('/tailwindcss/img/brand-img.png')",
+        'card-01-img':"url('/tailwindcss/tailwindcss/img/img-01.png')",
+        'card-02-img':"url('/tailwindcss/img/img-02.png')",
+        'card-03-img':"url('/tailwindcss/img/img-03.png')",
+        'card-04-img':"url('/tailwindcss/img/img-04.png')",
+        'card-05-img':"url('/tailwindcss/img/img-05.png')",
+        'sale-banner-img':"url('/tailwindcss/img/img-banner.png')",
+        'app-banner-img':"url('/tailwindcss/img/mobile-app.png')",
       },
 
    
